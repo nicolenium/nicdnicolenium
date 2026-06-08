5da37099188af090d1fd9c993de6bd9abe7f5231
nicolenium <nicolenium@hotmail.com>
2026-06-08 13:38:48 -0400

    Refactor MathGamesSetupPage to use GameSetup component

--- src/pages/MathGamesSetupPage.jsx
+++ src/pages/MathGamesSetupPage.jsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import GameSetupLayout from '@/components/GameSetupLayout.jsx';
+import GameSetup from '@/components/GameSetup'
 
 export default function MathGamesSetupPage() {
-  const navigate = useNavigate();
-
-  const handleStart = (settings) => {
-    navigate('/math-games-game', { state: { settings } });
-  };
-
   return (
-    <GameSetupLayout 
-      gameName="Math Challenge" 
-      gameType="math_challenge" 
-      onStart={handleStart} 
+    <GameSetup
+      gameId="math-challenge"
+      gameName="Math Challenge"
+      hasAI={false}
+      hasOnline={true}
     />
-  );
+  )
 }
