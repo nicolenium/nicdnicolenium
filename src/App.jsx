ae48884178a5b18a138d7dbc3dc3a8f663f2e550
nicolenium <nicolenium@hotmail.com>
2026-06-10 01:29:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 import GamePage from './pages/GamePage'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={
-        <div style={{background:'#020617',minHeight:'100vh',color:'white',padding:'40px'}}>
-          <h1 style={{fontSize:'3rem',color:'#818cf8'}}>NicdNicolenium</h1>
-          <p>Home Page</p>
-        </div>
-      }/>
+      <Route path="/" element={<HomePage />}/>
       <Route path="/game/:gameId" element={<GamePage />}/>
     </Routes>
   )
