241d42bf574b00c9a4e9e7091ec1d44d6387d324
nicolenium <nicolenium@hotmail.com>
2026-06-10 02:22:31 -0400

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { translations } from './i18n/translations.js';
+import { translations } from './i18n/translations.js'; // ← CHECK THIS PATH
 
 i18n
   .use(initReactI18next)
