a6191549bc2d87a77f6edbb6104afec8cbf0736f
nicolenium <nicolenium@hotmail.com>
2026-06-10 02:32:07 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import './i18n.js'  // ← THIS MUST BE FIRST
+import './i18n.js'
 import App from './App.jsx'
 import './index.css'
 import { I18nextProvider } from 'react-i18next'
@@ -9,7 +9,9 @@ import i18n from './i18n.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <Suspense fallback={<div style={{color: 'white', textAlign: 'center', marginTop: '20vh'}}>Loading...</div>}>
+        <App />
+      </Suspense>
     </I18nextProvider>
   </React.StrictMode>,
 )
