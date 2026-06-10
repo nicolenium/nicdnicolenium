5d63f28b5a0ce4ee49bbff2e006651858c6876d9
nicolenium <nicolenium@hotmail.com>
2026-06-10 02:18:04 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import './i18n.js'  // ← THIS MUST BE FIRST
 import App from './App.jsx'
 import './index.css'
-import './i18n.js'  // ← This loads your new i18n config
-import { I18nextProvider } from 'react-i18next'  // ← Add this
-import i18n from './i18n.js'  // ← Add this
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <I18nextProvider i18n={i18n}>  {/* ← Wrap App with this */}
-        <App />
-      </I18nextProvider>
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
 )
