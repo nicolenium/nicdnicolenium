37c6ef6952f7370080ecc20e7a1c3c27172a135f
nicolenium <nicolenium@hotmail.com>
2026-06-10 02:29:08 -0400

    Update apiServerClient.js

--- src/lib/apiServerClient.js
+++ src/lib/apiServerClient.js
@@ -1,11 +1,9 @@
 const API_SERVER_URL = '/hcgi/api';
 
 const apiServerClient = {
-    fetch: async (url, options = {}) => {
-        return await window.fetch(API_SERVER_URL + url, options);
-    }
+  fetch: async (url, options = {}) => {
+    return await window.fetch(API_SERVER_URL + url, options);
+  }
 };
 
 export default apiServerClient;
-
-export { apiServerClient };
